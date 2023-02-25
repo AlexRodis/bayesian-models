1f3dcead1ab189c5604a9bdc983bd93b2fbb9b8f
Alexander Rodis <alexanderrhodis@gmail.com>
2023-02-25 15:31:05 +0200

    added typing submodule

---
+++ bayesian_models/typing.py
@@ -0,0 +1,9 @@
+from typing import Any, Union, Optional
+from numpy.typing import NDArray
+from pandas import DataFrame
+from xarray import DataArray
+
+
+
+InputData = Union[DataArray, DataFrame, NDArray]
+ndarray = NDArray
